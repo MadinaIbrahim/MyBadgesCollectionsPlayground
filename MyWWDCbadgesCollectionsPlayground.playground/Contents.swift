cc14138c61100756c8cfdcd613b6d2ec03dff7ef
MadinaIbrahim <madina.ibrahim.us@gmail.com>
2016-10-07 16:19:05 -0400

    Solved

--- MyWWDCbadgesCollectionsPlayground.playground/Contents.swift
+++ MyWWDCbadgesCollectionsPlayground.playground/Contents.swift
@@ -1,8 +1,18 @@
 //: Playground - noun: a place where people can play
 
 import UIKit
+var speakersRoomsTable: [String:Int]? = ["Anita Borg":201,
+                                         "Alan Kay":202,
+                                         "Ada Lovelace":203,
+                                         "Aaron Swartz":215,
+                                         "Alan Turing":204,
+                                         "Michael Faraday":205,
+                                         "Grace Hopper":230,
+                                         "" : 245]
+
 
 func badgeForSpeaker(name: String) -> String {
+    
     return "Hello, my name is \(name)"
 }
 
@@ -13,36 +23,26 @@ func badgesForSpeakers(speakers: [String])-> [String] {
         let message = "Hello, my name is \(name)"
         badgesArray.append(message)
     }
-    print(badgesArray)
     return badgesArray
 }
 
 func roomAssignmentsForSpeakers(speaker:[String])-> [String]{
     
-    let speakersRoomsTable:[String:Int] = ["Anita Borg":201,
-                              "Alan Kay":202,
-                              "Ada Lovelace":203,
-                              "Aaron Swartz":215,
-                              "Alan Turing":204,
-                              "Michael Faraday":205,
-                              "Grace Hopper":230]
+    let roomAssignments = [String]()
     
-    var roomAssignments = [String]()
-    
-    for (key,value) in speakersRoomsTable {
-        let emptyKey = "\(speaker) has not been assigned to a room."
-        let descriptionMessage = "\(key) will be speaking in Room \(value)."
+    for person in speaker
+    {
+        let room = speakersRoomsTable![person]
         
-        if key == "" {
-            roomAssignments.append(emptyKey)
-            return roomAssignments
+        if let roomNumber = room {
+            print("\(person) will be speaking in Room \(roomNumber).")
         } else {
-            roomAssignments.append(descriptionMessage)
-                }
-            }
-    print(roomAssignments)
+            print("\(person) has not been assigned to a room.")
+        }
+    }
     return roomAssignments
 }
+
 
 badgeForSpeaker("Anita Borg")
 badgesForSpeakers(["Anita Borg", "Alan Kay", "Ada Lovelace", "Aaron Swartz", "Alan Turing", "Michael Faraday", "Grace Hopper"])
